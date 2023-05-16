80dff339b27d95d0e6972bd90d5c7a18dc70a4e0
Da-cruz <wilfrieddacruz@arolitec.com>
2023-05-16 17:47:00 +0000

    create css file for styling hotel list

--- src/app/hotel-list/hotel-list.component.ts
+++ src/app/hotel-list/hotel-list.component.ts
@@ -4,6 +4,7 @@ import { IHotel } from './hotel';
 @Component({
   selector: 'app-hotel-list',
   templateUrl: './hotel-list.component.html',
+  styleUrls: ['./hotel-list.component.css'],
 })
 export class HotelListComponent implements OnInit {
   public title = 'liste hotels';
