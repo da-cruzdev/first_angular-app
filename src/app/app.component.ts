32a2172edd30741a92af3deca43020df4eb2ec83
Da-cruz <wilfrieddacruz@arolitec.com>
2023-05-16 14:26:47 +0000

    installing bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  template: ` <h1>{{ title }}</h1> `,
+  styleUrls: [],
 })
 export class AppComponent {
-  title = 'Willdev';
+  title: string = 'hostel app';
 }
