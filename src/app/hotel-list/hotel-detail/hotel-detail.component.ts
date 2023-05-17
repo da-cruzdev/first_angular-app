a97dee2b5848ada3c2db35e538fc0381ba000300
Da-cruz <wilfrieddacruz@arolitec.com>
2023-05-17 14:44:36 +0000

    add detail page for hotels

--- src/app/hotel-list/hotel-detail/hotel-detail.component.ts
+++ src/app/hotel-list/hotel-detail/hotel-detail.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HotelListService } from '../hotel-list.service';
+import { IHotel } from '../hotel';
 
 @Component({
   selector: 'app-hotel-detail',
   templateUrl: './hotel-detail.component.html',
-  styleUrls: ['./hotel-detail.component.css']
+  styleUrls: ['./hotel-detail.component.css'],
 })
-export class HotelDetailComponent {
+export class HotelDetailComponent implements OnInit {
+  constructor(
+    private route: ActivatedRoute,
+    private readonly hotelService: HotelListService
+  ) {}
 
+  public hotel: IHotel = <IHotel>{};
+
+  ngOnInit(): void {
+    const idParam = this.route.snapshot.paramMap.get('id');
+    const id: number = idParam ? +idParam : 0;
+    this.hotelService.getHotels().subscribe((hotels: IHotel[]) => {
+      this.hotel = hotels.find((h) => h.hotelId === id) || <IHotel>{};
+
+      console.log('hotels: ', this.hotel);
+    });
+  }
 }
