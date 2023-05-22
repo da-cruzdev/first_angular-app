931047c579325f7dfc84cdccfca5e515bcce7e29
Da-cruz <wilfrieddacruz@arolitec.com>
2023-05-22 14:12:35 +0000

    add subscription and onDestroy

--- src/app/hotels/hotel-detail/hotel-detail.component.ts
+++ src/app/hotels/hotel-detail/hotel-detail.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { HotelListService } from '../shared/services/hotel-list.service';
 import { IHotel } from '../shared/models/hotel';
+import { map } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-hotel-detail',
   templateUrl: './hotel-detail.component.html',
   styleUrls: ['./hotel-detail.component.css'],
 })
-export class HotelDetailComponent implements OnInit {
+export class HotelDetailComponent implements OnInit, OnDestroy {
   constructor(
     private route: ActivatedRoute,
     private readonly hotelService: HotelListService,
@@ -17,14 +19,27 @@ export class HotelDetailComponent implements OnInit {
 
   public hotel: IHotel = <IHotel>{};
 
+  public subscription: Subscription = new Subscription();
+
   ngOnInit(): void {
     const idParam = this.route.snapshot.paramMap.get('id');
     const id: number = idParam ? +idParam : 0;
-    this.hotelService.getHotels().subscribe((hotels: IHotel[]) => {
-      this.hotel = hotels.find((hotel) => hotel.id === id) || <IHotel>{};
+    this.subscription.add(
+      this.hotelService
+        .getHotels()
+        .pipe(
+          map((hotels: IHotel[]) => hotels.find((hotel) => hotel.id === id))
+        )
+        .subscribe((hotel: IHotel | undefined) => {
+          if (hotel) this.hotel = hotel;
 
-      console.log('hotels: ', this.hotel);
-    });
+          console.log('hotels: ', this.hotel);
+        })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
   public backToList(): void {
